86da1890b4c78749995102e5f154b37b9a9efe37
Michael Kainer <kaini@pushrax.com>
2017-03-09 12:33:54 +0100

    Make test, debug and release build working again

--- timekeeper/src/debug.rs
+++ timekeeper/src/debug.rs
@@ -1,4 +1,4 @@
-#[cfg(debug_assertions)]
+#[cfg(all(debug_assertions, not(test)))]
 pub unsafe fn write0(ptr: *const u8) {
     // See newlib for details (libgloss/arm/swi.h)
     asm!(
@@ -9,14 +9,21 @@ pub unsafe fn write0(ptr: *const u8) {
     );
 }
 
-#[cfg(not(debug_assertions))]
+#[cfg(any(not(debug_assertions), test))]
 pub unsafe fn write0(_ptr: *const u8) {
 }
 
+#[cfg(all(debug_assertions, not(test)))]
 macro_rules! breakpoint {
-    () => (unsafe { asm!("bkpt" :::: "volatile") })
+    () => (unsafe { asm!("bkpt" :::: "volatile"); });
 }
 
+#[cfg(any(not(debug_assertions), test))]
+macro_rules! breakpoint {
+    () => ();
+}
+
+#[cfg(debug_assertions)]
 macro_rules! debug {
     ($fmt:expr, $($args:tt)*) => ({
         let s = ::collections::fmt::format(format_args!(concat!($fmt, "\n\0"), $($args)*));
@@ -25,6 +32,12 @@ macro_rules! debug {
     ($msg:expr) => ({
         debug!("{:?}", $expr);
     });
+}
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ($fmt:expr, $($args:tt)*) => ();
+    ($msg:expr) => ();
 }
 
 macro_rules! debug_trace {
