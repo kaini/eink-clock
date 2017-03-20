3e3629cbcd9d1959dc3403d29438416b503da3e3
Michael Kainer <kaini@pushrax.com>
2017-03-20 21:25:21 +0100

    Remove libc

--- my_allocator/src/lib.rs
+++ my_allocator/src/lib.rs
@@ -1,41 +1,59 @@
 #![feature(allocator)]
+#![feature(const_fn)]
 #![allocator]
 #![no_std]
 #![crate_type="rlib"]
 #![crate_name="my_allocator"]
 
-use core::ptr;
+extern crate linked_list_allocator;
 
-const ALIGN: usize = 8;
+use linked_list_allocator::Heap;
+use core::intrinsics::copy_nonoverlapping;
+use core::ptr::null_mut;
+use core::cmp::min;
 
-extern {
-    fn malloc(size: usize) -> *mut u8;
-    fn free(ptr: *mut u8);
-    fn realloc(ptr: *mut u8, size: usize) -> *mut u8;
+static mut HEAP: Heap = Heap::empty();
+
+pub fn init() {
+    extern {
+        static __heap_start__: u8;
+        static __heap_end__: u8;
+    }
+    unsafe {
+        HEAP.init(
+            &__heap_start__ as *const u8 as usize,
+            &__heap_end__ as *const u8 as usize - &__heap_start__ as *const u8 as usize);
+    }
 }
 
 #[no_mangle]
 pub extern fn __rust_allocate(size: usize, align: usize) -> *mut u8 {
-    if align > ALIGN {
-        ptr::null_mut()
-    } else {
-        unsafe {
-            malloc(size)
+    unsafe {
+        if let Some(ptr) = HEAP.allocate_first_fit(size, align) {
+            ptr
+        } else {
+            null_mut()
         }
     }
 }
 
 #[no_mangle]
-pub extern fn __rust_deallocate(ptr: *mut u8, _old_size: usize, _align: usize) {
+pub extern fn __rust_deallocate(ptr: *mut u8, old_size: usize, align: usize) {
     unsafe {
-        free(ptr);
+        HEAP.deallocate(ptr, old_size, align);
     }
 }
 
 #[no_mangle]
-pub extern fn __rust_reallocate(ptr: *mut u8, _old_size: usize, size: usize, _align: usize) -> *mut u8 {
+pub extern fn __rust_reallocate(ptr: *mut u8, old_size: usize, size: usize, align: usize) -> *mut u8 {
     unsafe {
-        realloc(ptr, size)
+        if let Some(new_ptr) = HEAP.allocate_first_fit(size, align) {
+            copy_nonoverlapping(ptr, new_ptr, min(old_size, size));
+            HEAP.deallocate(ptr, old_size, align);
+            new_ptr
+        } else {
+            null_mut()
+        }
     }
 }
 
